142c7a44e4d93af28420900873493250c0f4e02d
Anas Almasri <Anaskhaldoun2@gmail.com>
2025-01-19 15:14:49 -0500

    Updated to include actual phone dataset

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -14,12 +14,12 @@ import { MdOutlineChatBubbleOutline } from "react-icons/md";
 const Footer = () => {
   return (
     <Flex h="3em" color="white" backgroundColor="#FF006B" alignItems="center" justifyContent="space-between">
-        <Text pl={5}>©PhoneBuddy</Text>
+        <Text pl={5}>PhoneBuddy</Text>
         <Flex w="20em" pr={5} justifyContent="space-around">
-            <Icon as={LuPhone} w={6} h={6} />
+            {/* <Icon as={LuPhone} w={6} h={6} />
             <Icon as={MdMailOutline} w={6} h={6} />
             <Icon as={MdOutlineChatBubbleOutline} w={6} h={6} />
-            <Icon as={FaInstagram} w={6} h={6} />
+            <Icon as={FaInstagram} w={6} h={6} /> */}
         </Flex>
     </Flex>
   )
--- src/pages/components/Results.jsx
+++ src/pages/components/Results.jsx
@@ -19,7 +19,7 @@ const Results = ({ filter, sort, setMessage }) => {
   const handlePageChange = (newPage) => {
     setPage(newPage);
     if (resultsRef.current) {
-      resultsRef.current.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      resultsRef.current.scrollIntoView({ behavior: 'smooth', block: 'start' }); 
     }
   };
 
@@ -40,15 +40,16 @@ const Results = ({ filter, sort, setMessage }) => {
     <>
       <SimpleGrid
         ref={resultsRef} // Attach ref to the results container
+        scrollMarginTop={'9em'} // Scroll margin to prevent the fixed header from overlapping the results
         minChildWidth="15em"
         spacing={{ sm: '5', md: '10' }}
         mt="2em"
         ml="2em"
         mr="2em"
       >
-        {/* {phones.length > 0 ? phoneBoxes : <h1>No phones found</h1>} */}
-        <PhoneBox key={0} id={0} image="https://images.ctfassets.net/evnrpfa1vdk4/19ppXPEmEtAWZfb8eBcIdJ/94a86c4f021984f4ae87d1ce69363cf4/iPhone_15_Pro_Max_Natural_Titanium_PDP_Image_Position-1__CAEN-copy.png?w=324&q=80&fm=webp" 
-        brand={"apple"} phone={"iPhone 15 Pro Max to the Max!"} price={"21.50"} upfront={"300"} provider={"Kootelus"} />
+        {phones.length > 0 ? phoneBoxes : <h1>No phones found</h1>}
+        {/* <PhoneBox key={0} id={0} image="https://images.ctfassets.net/evnrpfa1vdk4/19ppXPEmEtAWZfb8eBcIdJ/94a86c4f021984f4ae87d1ce69363cf4/iPhone_15_Pro_Max_Natural_Titanium_PDP_Image_Position-1__CAEN-copy.png?w=324&q=80&fm=webp" 
+        brand={"apple"} phone={"iPhone 15 Pro Max to the Max!"} price={"21.50"} upfront={"300"} provider={"Kootelus"} /> */}
       </SimpleGrid>
       <Flex justifyContent="center" mt="2em">
         <Button isDisabled={page === 1} onClick={() => handlePageChange(page - 1)}>Previous</Button>
